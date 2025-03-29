44ca45776143d5a5748f911478934463df9b13cb
Priyanka Mehta <primehta17@gmail.com>
2025-03-29 20:02:59 +0530

    fourth commit

---
+++ three.js
@@ -0,0 +1,11 @@
+//length of array without array
+let arr=[9,33,2,33,4,77];
+let i=0,count=0;
+while(arr[i]!=undefined){
+    count++;
+    i++;
+    // console.log(count)
+}
+
+console.log(count);
+
